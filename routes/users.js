659910edbfcf9de9bf098cde7331bb0f30126394
saugat-15 <saugatgiri15@gmail.com>
2026-06-18 11:55:28 +1000

    inject secret from key vault through key vault reference

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var dotenv = require("dotenv");
+dotenv.config();
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.send("respond with a resources" + process.env.KEYVAULT_SECRET);
 });
 
 module.exports = router;
